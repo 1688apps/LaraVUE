654abc1e1c105954d7bd954f7462274ec0ce4a46
1688apps <1688apps@gmail.com>
2020-11-29 20:50:43 +0800

    failed to show

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 
 Route::resource('/mobiles', 'MobileController'); 
 
-
-});
+Route::get('/mobiles', function () {
+    return view('mobiles');
