64db8bc8b644f6e152158c255bbd032ad6d249b3
1688apps <1688apps@gmail.com>
2020-11-29 19:41:50 +0800

    home.blade; require vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+window.Vue = require('vue');
+Vue.component('example', require('./components/Example.vue'));
+Vue.component('mobiles', require('./components/Mobile.vue'));
+const app = new Vue({
+    el: '#app'
+});
